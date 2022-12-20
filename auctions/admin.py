cfe88aff04d3562d4dae0f0dd76774631e77b5d0
Brian Chalfant <brian.chalfant@gmail.com>
2022-12-19 23:02:56 -0500

    added models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from auctions.models import Comment, User, Bid, Listing
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(User)
+admin.site.register(Bid)
+admin.site.register(Listing)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,33 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    startingPrice = models.IntegerField()
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=5000)
+
+    class Condition(models.TextChoices):
+        NEW = 'New'
+        USED = 'Used'
+        REFURB = 'Refurbished'
+        NW = 'Not Working/For Parts'
+
+    condition = models.CharField(
+        max_length=21,
+        choices = Condition.choices,
+        default=Condition.NEW
+    )
+
+class Bid(models.Model):
+    amount = models.IntegerField()
+    listing = Listing()
+    user = User()
+    date = models.DateField()
+    time = models.TimeField()
+
+class Comment(models.Model):
+    text = models.CharField(max_length=255)
+    user = User()
+    date = models.DateField()
+    time = models.TimeField()
